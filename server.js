1bbf7e8a588f059923eb423ee92a733ab54e1427
WiteRive <witerive@yandex.ru>
2026-04-22 02:55:16 +0500

    wrw

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(express.static("public"));
 
 mongoose.connect(config.MONGO_URL);
 
+let online = {}; // socket.id -> username
+
 /* AUTH */
 app.post("/register", async (req,res)=>{
     const {username,password}=req.body;
@@ -59,11 +61,12 @@ io.use((socket,next)=>{
         socket.username=data.username;
         next();
     }catch(e){
-        next(new Error("auth failed"));
+        next(new Error("auth"));
     }
 });
 
-let online = {};
+/* STATE */
+let typingUsers = {};
 
 io.on("connection",(socket)=>{
 
@@ -71,27 +74,26 @@ io.on("connection",(socket)=>{
 
     emitUsers();
 
+    /* JOIN */
     socket.on("join",()=>{
         emitUsers();
     });
 
+    /* MESSAGE */
     socket.on("private_message", async (data)=>{
 
         const msg = await Message.create({
             from:socket.username,
             to:data.to,
-            message:data.message
+            message:data.message,
+            status:"sent"
         });
 
-        for(let id in online){
-            if(online[id]===data.to){
-                io.to(id).emit("private_message",msg);
-            }
-        }
-
+        emitToUser(data.to,"private_message",msg);
         socket.emit("private_message",msg);
     });
 
+    /* HISTORY */
     socket.on("get_history", async (user)=>{
 
         const msgs = await Message.find({
@@ -104,24 +106,61 @@ io.on("connection",(socket)=>{
         socket.emit("chat_history",msgs);
     });
 
+    /* READ RECEIPTS */
+    socket.on("read_messages", async (data)=>{
+
+        await Message.updateMany(
+            {from:data.from,to:data.to},
+            {$set:{status:"read"}}
+        );
+
+        emitToUser(data.from,"messages_read",{
+            from:data.from,
+            to:data.to
+        });
+    });
+
+    /* TYPING */
+    socket.on("typing", (to)=>{
+
+        emitToUser(to,"typing",{
+            from:socket.username
+        });
+    });
+
+    socket.on("stop_typing",(to)=>{
+        emitToUser(to,"stop_typing",{
+            from:socket.username
+        });
+    });
+
+    /* DISCONNECT */
     socket.on("disconnect",()=>{
 
         delete online[socket.id];
+
         emitUsers();
     });
 
 });
 
-/* USERS BROADCAST */
-async function emitUsers(){
-    const users = await User.find({}, "username");
+/* HELPERS */
+function emitUsers(){
+    const users = Object.values(online);
 
     io.emit("users",{
-        users,
-        online:Object.values(online)
+        users
     });
 }
 
+function emitToUser(username,event,data){
+    for(let id in online){
+        if(online[id]===username){
+            io.to(id).emit(event,data);
+        }
+    }
+}
+
 server.listen(3000,()=>{
-    console.log("FIXED CHAT RUN");
+    console.log("PRO STABLE 4 RUN");
 });
