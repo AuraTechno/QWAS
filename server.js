5746410efb79d344f727e1d7bfc7c8130c8eca67
WiteRive <witerive@yandex.ru>
2026-04-22 02:29:56 +0500

    укцу

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const config = require("./config");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,9 +11,8 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static(__dirname));
 
-mongoose.connect(config.MONGO_URL);
+mongoose.connect("mongodb+srv://server:PASSWORD@qwas.ijvw0zw.mongodb.net/messenger");
 
-/* MODELS */
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
@@ -33,7 +31,7 @@ const MessageSchema = new mongoose.Schema({
 const User = mongoose.model("User", UserSchema);
 const Message = mongoose.model("Message", MessageSchema);
 
-let online = {}; // socketId -> username
+let online = {};
 
 /* AUTH */
 app.post("/register", async (req,res)=>{
@@ -70,6 +68,7 @@ app.post("/login", async (req,res)=>{
 io.on("connection",(socket)=>{
 
     socket.on("join", async (username)=>{
+
         online[socket.id]=username;
 
         const users = await User.find({}, "username avatar lastSeen");
@@ -80,7 +79,6 @@ io.on("connection",(socket)=>{
         });
     });
 
-    /* SEND MESSAGE */
     socket.on("private_message", async (data)=>{
 
         const from = online[socket.id];
@@ -92,18 +90,15 @@ io.on("connection",(socket)=>{
             status:"sent"
         });
 
-        // deliver to receiver
         for(let id in online){
             if(online[id]===data.to){
                 io.to(id).emit("private_message",msg);
             }
         }
 
-        // echo to sender
         socket.emit("private_message",msg);
     });
 
-    /* HISTORY */
     socket.on("get_history", async (withUser)=>{
 
         const user = online[socket.id];
@@ -118,51 +113,10 @@ io.on("connection",(socket)=>{
         socket.emit("chat_history",msgs);
     });
 
-    /* READ FIX (IMPORTANT) */
-    socket.on("mark_read", async (data)=>{
-
-        const {from,to} = data;
-
-        await Message.updateMany(
-            {from:from,to:to,status:{$ne:"read"}},
-            {$set:{status:"read"}}
-        );
-
-        // 🔥 ВАЖНО: уведомляем ТОЛЬКО отправителя
-        for(let id in online){
-            if(online[id]===from){
-                io.to(id).emit("message_read",{
-                    from,
-                    to
-                });
-            }
-        }
-    });
-
-    /* TYPING */
-    socket.on("typing",(data)=>{
-        for(let id in online){
-            if(online[id]===data.to){
-                io.to(id).emit("typing",{from:data.from});
-            }
-        }
-    });
-
-    /* DISCONNECT */
-    socket.on("disconnect", async ()=>{
-
-        const user = online[socket.id];
-
-        if(user){
-            await User.updateOne(
-                {username:user},
-                {$set:{lastSeen:Date.now()}}
-            );
-        }
-
+    socket.on("disconnect",()=>{
         delete online[socket.id];
     });
 
 });
 
-server.listen(3000,()=>console.log("PRO 3.4 RUN"));
+server.listen(3000,()=>console.log("STABLE RUN"));
