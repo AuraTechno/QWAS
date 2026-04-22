cfd7cd7f3c6a042c976d85f898a2c8599baa49a2
WiteRive <witerive@yandex.ru>
2026-04-22 23:39:28 +0500

    куые

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGO_URL: "mongodb+srv://server:bRtteM2rqijlDTsd@qwas.ijvw0zw.mongodb.net/messenger?retryWrites=true&w=majority",
-  JWT_SECRET: "secret_key_123"
+  JWT_SECRET: "secret_key_123",
   MESSAGES_PER_PAGE: 30
-};
+}
