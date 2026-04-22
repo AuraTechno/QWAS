c520057ae06625739b15540aba51d3a54c1f8fae
WiteRive <witerive@yandex.ru>
2026-04-22 23:38:28 +0500

    йцу

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // Добавляем retryWrites и w=majority для стабильности
   MONGO_URL: "mongodb+srv://server:bRtteM2rqijlDTsd@qwas.ijvw0zw.mongodb.net/messenger?retryWrites=true&w=majority",
   JWT_SECRET: "secret_key_123"
-  MESSAGES_PER_PAGE: 30  // Количество сообщений за одну загрузку
+  MESSAGES_PER_PAGE: 30
 };
